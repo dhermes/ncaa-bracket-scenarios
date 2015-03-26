64320c663f34e56934f3494f2428061d313d9e7e
Danny Hermes <daniel.j.hermes@gmail.com>
2015-03-26 07:42:50 -0700

    Adding first step of pipeline.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ncaa-bracket-scenarios
 Toy Project to Parse NCAA Group and Create Scenarios
+
+## Pipeline For Computing Scenarios
+
+1. Get the current year's field by running `get_team_mapping.py`. This
+   may require updating the `NATIONAL_BRACKET` and the HTML parsers
+   which pick out the teams.
---
+++ get_team_mapping.py
@@ -0,0 +1,54 @@
+from BeautifulSoup import BeautifulSoup
+import base64
+import json
+import os
+import requests
+
+
+NATIONAL_BRACKET = ('http://games.espn.go.com/tournament-challenge-bracket'
+                    '/2015/en/nationalBracket')
+FILENAME = base64.b64encode(NATIONAL_BRACKET) + '.html'
+
+
+def get_national_bracket():
+    if not os.path.exists(FILENAME):
+        response = requests.get(NATIONAL_BRACKET)
+        with open(FILENAME, 'w') as fh:
+            fh.write(response.content)
+        response.close()
+    with open(FILENAME, 'r') as fh:
+        return fh.read()
+
+
+def get_team_info(data_tag):
+    """Returns ({teamID}, {team name}) from a node."""
+    name_span, = data_tag.findAll('span', {'class': 'name'})
+    team_name = name_span.text
+    team_id = int(data_tag['data-slotindex'])
+    return team_id, team_name
+
+
+def get_data_slot_tags(tag):
+    if tag.name != 'div':
+        return False
+    return tag.has_key('data-slotindex')
+
+
+def parse_teams():
+    bracket_html = get_national_bracket()
+    soup = BeautifulSoup(bracket_html)
+
+    data_tags = soup.findAll(get_data_slot_tags)
+    opening_round_tags = [tag for tag in data_tags
+                          if int(tag['data-slotindex']) < 64]
+    team_info = [get_team_info(data_tag) for data_tag in
+                 opening_round_tags]
+    team_info = dict(set(team_info))
+
+    with open('team_map.json', 'w') as fh:
+        json.dump(team_info, fh, indent=2, sort_keys=True,
+                  separators=(',', ': '))
+
+
+if __name__ == '__main__':
+    parse_teams()
