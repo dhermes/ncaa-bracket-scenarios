c04e65c35409ba649992556c1caeb90663a401b3
Danny Hermes <daniel.j.hermes@gmail.com>
2017-03-15 21:52:34 -0700

    Running step 1 of the README pipeline.

--- get_team_mapping.py
+++ get_team_mapping.py
@@ -8,6 +8,7 @@ import requests
 NATIONAL_BRACKET = ('http://games.espn.com/tournament-challenge-bracket/'
                     '2017/en/entry?entryID=115703')
 FILENAME = base64.b64encode(NATIONAL_BRACKET) + '.html'
+SLOT_KEY = 'data-slotindex'
 
 
 def get_national_bracket():
@@ -24,16 +25,16 @@ def get_team_info(data_tag):
     """Returns ({teamID}, {team name}) from a node."""
     name_span, = data_tag.findAll('span', {'class': 'name'})
     team_name = name_span.text
-    slot_id = int(data_tag['data-slotindex'])
+    slot_id = int(data_tag[SLOT_KEY])
     # NOTE: Assumes the team ID is 1 more than the slot ID.
     team_id = slot_id + 1
     return team_id, team_name
 
 
 def get_data_slot_tags(tag):
-    if tag.name != 'div':
+    if tag.name != 'a':
         return False
-    return tag.has_key('data-slotindex')
+    return tag.has_key(SLOT_KEY)
 
 
 def parse_teams():
@@ -42,7 +43,8 @@ def parse_teams():
 
     data_tags = soup.findAll(get_data_slot_tags)
     opening_round_tags = [tag for tag in data_tags
-                          if int(tag['data-slotindex']) < 64]
+                          if int(tag[SLOT_KEY]) < 64]
+    assert len(opening_round_tags) == 64
     team_info = [get_team_info(data_tag) for data_tag in
                  opening_round_tags]
     team_info = dict(set(team_info))
